f1255099662bbded169bc3e98a61090e78f4b983
ThaisAlves02 <thaisgraziele56@gmail.com>
2026-07-08 13:47:26 -0300

    update

---
+++ UC 2/Programação Orientada a Objeto/Aula 9/classSalaodeBeleza.py
@@ -0,0 +1,25 @@
+class Cliente():
+    def __init__(self, nome, cpf, telefone, email):
+       self.nome = nome
+       self.cpf = cpf
+       self.telefone = telefone
+       self.email = email
+       
+    
+class Servico:
+    def __init__(self, nome, valor, duracao):
+        self.nome = nome
+        self.valor = valor
+        self.duracao = duracao
+
+
+class Agendamento:
+    def __init__(self, cliente, servico, data, horario):
+        self.cliente = cliente
+        self.servico = servico
+        self.data = data
+        self.horario = horario
+
+    
+
+
--- UC 2/Programação Orientada a Objeto/Aula 9/main.py
+++ UC 2/Programação Orientada a Objeto/Aula 9/main.py
@@ -1,10 +1,177 @@
-from classCliente import Cliente
+from classSalaodeBeleza import Cliente, Servico, Agendamento
 
-cliente1 = Cliente("Luíza", "02948349209", "85924356424", "ninguem@gmail.com")
-cliente2 = Cliente("João", "12345678900", "85999999999", "joao@gmail.com")
+clientes = []
+servicos = []
+agendamentos = []
 
-cliente1.cadastrar_cliente()
-cliente2.cadastrar_cliente()
+def cadastrar_cliente():
+    print("=== CADASTRO DE CLIENTES ===")
+    nome = input("Nome: ")
+    
+    cliente_existente = False
+    for nome_cliente in clientes:
+        if nome_cliente.nome == nome:
+            cliente_existente = True
+            break
 
-for cliente in Cliente.novocliente:
-    cliente.exibir_cliente()
+
+    if cliente_existente:
+        print(f"O Cliente {nome} ja esta cadastrado")
+    else:
+        cpf = input("CPF: ")
+        telefone = input("Telefone: ")
+        email = input("E-mail: ")
+
+        cliente = Cliente(nome, cpf, telefone, email)
+        clientes.append(cliente)
+
+        print()
+        print("Cliente cadastrado com sucesso!")
+
+def cadastrar_servico():
+    print("=== CADASTRO DE SERVIÇOS ===")
+
+    nome = input("Nome: ")
+    
+    servico_existente = False
+    for nome_servico in servicos:
+        if nome_servico.nome == nome:
+            servico_existente = True
+            break
+    
+    if servico_existente:
+        print(f"O serviço {nome} ja esta cadastrado")
+    else:
+        while True:
+            valor = float(input("Valor: "))
+            if valor > 0:
+                break
+            print("O valor deve ser maior que zero.")
+
+        duracao = int(input("Duração (minutos): "))
+
+        servico = Servico(nome, valor, duracao)
+        servicos.append(servico)
+
+    
+def exibir_cliente():
+    print("VIZUALIZAR CLIENTES")
+         
+    if not clientes:
+        print("Nenhum cliente cadastrado.")
+        
+    for i, cliente in enumerate(clientes):
+        print(f"{i+1} - {cliente.nome}")
+
+
+def exibir_servico():
+    if not servicos:
+       print("Nenhum serviço cadastrado.")
+    
+    for i, servico in enumerate(servicos):
+        print(f"{i+1} - {servico.nome}")
+
+
+def agendar():
+    print("=== AGENDAR ATENDIMENTO ===")
+
+    if len(clientes) == 0:
+        print("Cadastre um cliente primeiro.")
+      
+    if len(servicos) == 0:
+        print("Cadastre um serviço primeiro.")
+
+    exibir_cliente()
+
+    num_cliente = int(input("Digite o número do cliente: "))
+
+    if num_cliente < 1 or num_cliente > len(clientes):
+        print("Cliente inválido!")
+    
+    exibir_servico()
+
+    num_servico = int(input("Digite o número do serviço: "))
+
+    if num_servico < 1 or num_servico > len(servicos):
+        print("Serviço inválido!")
+
+    data = input("Data (dd/mm/aaaa): ")
+    horario = input("Horário (HH:MM): ")
+
+    for agenda in agendamentos:
+        if agenda.data == data and agenda.horario == horario:
+            print("Já existe um agendamento nesse horário!")
+    
+    novoAgn = Agendamento(
+        clientes[num_cliente-1],
+        servicos[num_servico-1],
+        data,
+        horario
+    )
+
+    agendamentos.append(novoAgn)
+
+
+def exibir_agendamento():
+    print(f"VIZUALIZAR AGENDAMENTOS")
+
+    if not agendamentos:
+        print("Nenhum agendamento.")
+
+    for i, agn in enumerate(agendamentos):
+        print(f"{i+1} - Cliente: {agn.cliente.nome} | Serviço: {agn.servico.nome} | Data: {agn.data} | Horário: {agn.horario}")
+
+
+def cancelar_agendamento():
+    print("\n=== Cancelar Agendamento ===")
+
+    if not agendamentos:
+        print("Nenhum agendamento cadastrado.\n")
+        return
+
+    exibir_agendamento()
+
+    num = int(input("Digite o número do agendamento: "))
+    num -=1
+    
+    if 0 <= num < len(agendamentos):
+        agendamentos.pop(num)
+        print("Agendamento cancelado!")
+    else:
+        print("Número inválido.")
+
+
+while True:
+
+    print("""======== SALÃO DE BELEZA ========
+            1 - Cadastrar Cliente
+            2 - Cadastrar Serviço
+            3 - Agendar Atendimento
+            4 - Listar Agendamentos
+            5 - Cancelar Agendamento
+            0 - Sair
+                """)
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        cadastrar_cliente()
+
+    elif opcao == "2":
+        cadastrar_servico()
+
+    elif opcao == "3":
+        agendar()
+
+    elif opcao == "4":
+        exibir_agendamento()
+
+    elif opcao == "5":
+        cancelar_agendamento()
+
+    elif opcao == "0":
+        print("Encerrando o programa...")
+        break
+
+    else:
+        print("Opção inválida.")
