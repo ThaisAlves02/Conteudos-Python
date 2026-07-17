5ae26a3e41fa2c39ba45f328c8125b45af323313
ThaisAlves02 <thaisgraziele56@gmail.com>
2026-07-17 20:48:17 -0300

    update

---
+++ UC 2/Engenharia de Software/Aula 7/classBola.py
@@ -0,0 +1,8 @@
+class Bola():
+    def __init__(self, cor, circunferência, material):
+        self.cor = cor
+        self.circunferência = circunferência
+        self.material = material
+
+    def trocar_cor(self):
+        
