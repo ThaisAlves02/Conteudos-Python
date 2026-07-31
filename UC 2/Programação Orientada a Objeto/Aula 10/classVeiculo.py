47d356861354fb2c68b53f80d2571942814205c4
ThaisAlves02 <thaisgraziele56@gmail.com>
2026-07-31 20:28:26 -0300

    update

--- UC 2/Programação Orientada a Objeto/Aula 10/classVeiculo.py
+++ UC 2/Programação Orientada a Objeto/Aula 10/classVeiculo.py
@@ -1,4 +1,4 @@
-class Veiculo ():
+class Veiculo():
     nome_estacionamento = "Shopping Central"
     valor_hora = 8.0 
 
@@ -7,3 +7,27 @@ class Veiculo ():
        self.modelo = modelo
        self.proprietario = proprietario
        self.horas_estacionadas = horas_estacionadas
+
+    def adicionar_horas(self):
+        
+            hora_adicionada = int(input("Digite a quantidade de horas estacionadas: "))
+
+            self.horas_estacionadas = self.horas_estacionadas + hora_adicionada
+
+            print(f"HORAS: {self.horas_estacionadas}")
+
+
+    def calcular_valor_total(self):
+        total = self.horas_estacionadas * self.valor_hora
+
+        print(f"Valor total: {total:.2f}")
+
+
+    def exibir_dados(self):
+            print(f"""
+        == DADOS DO VEÍCULO ==
+        Placa: {self.placa}
+        Modelo: {self.modelo}
+        Proprietário: {self.proprietario}
+        Horas Estacionadas: {self.horas_estacionadas}
+        """)
--- UC 2/Programação Orientada a Objeto/Aula 10/main.py
+++ UC 2/Programação Orientada a Objeto/Aula 10/main.py
@@ -1,24 +1,13 @@
 from classVeiculo import Veiculo
 
-def adicionar_horas(self):
-        hora_adicionada = int(input("Digite a quantidade de horas estacionadas: "))
-
-        self.horas_estacionadas = self.horas_estacionadas + hora_adicionada
-
-        print(f"HORAS: {self.horas_estacionadas}")
-
-
-def calcular_valor_total(self):
-       total = self.horas_estacionadas * self.valor_hora
-
-       print(f"Valor total: {total}")
+carro = Veiculo(
+    "ABC-1234",
+    "Gol",
+    "João",
+    5.0
+)
 
 
-def exibir_dados(self):
-        print(f"""
-    == DADOS DO VEÍCULO ==
-    Placa: {self.placa}
-    Modelo: {self.modelo}
-    Proprietário: {self.proprietario}
-    Horas Estacionadas: {self.horas_estacionadas}
-    """)
+carro.exibir_dados()
+carro.adicionar_horas()
+carro.calcular_valor_total()
